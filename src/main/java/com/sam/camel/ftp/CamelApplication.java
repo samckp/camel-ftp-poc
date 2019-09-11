43c36c7e98b17c8f3d03ffd7c4c1351e7218607e
samckp <sam.mca.liet@gmail.com>
2019-09-11 19:25:13 +0530

    Ftp route added

---
+++ src/main/java/com/sam/camel/ftp/CamelApplication.java
@@ -0,0 +1,14 @@
+package com.sam.camel.ftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(value = "com.sam.camel.route")
+public class CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelApplication.class, args);
+	}
+}
